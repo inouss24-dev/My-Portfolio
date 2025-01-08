bc974907a80ef60aeacb0d20b02268c347cba7e5
inouss24-dev <mrinouss24@gmail.com>
2025-01-08 10:51:41 +0000

    add project calculator

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,11 +2,9 @@ import Quiz from "../../public/Quiz App.png";
 import TodoList from "../../public/Todo List.png";
 import WeatherApp from "../../public/Weather App.png";
 import MyPortfolio from "../../public/Portfolio.png";
-import Calculator from "../../public/Calculator-react.png"
+import Calculator from "../../public/Calculator-react.png";
 import { FaPlay } from "react-icons/fa";
 import { AiFillGithub } from "react-icons/ai";
-
-
 
 const project = [
   {
@@ -42,8 +40,7 @@ const project = [
   {
     id: 4,
     title: "My Portfolio",
-    description:
-      "Mon Portfolio un espace qui fait ma biographie",
+    description: "Mon Portfolio un espace qui fait ma biographie",
     image: MyPortfolio,
     demo: "#navbar",
     gitHub: "#",
@@ -52,19 +49,21 @@ const project = [
   {
     id: 5,
     title: "Great Calculator",
-    description:
-      "Plonnger dans le monde du calcul avec cette outils",
+    description: "Plonnger dans le monde du calcul avec cette outils",
     image: Calculator,
     demo: "https://calculator-react-theta-one.vercel.app/",
     gitHub: "https://github.com/inouss24-dev/calculator-react",
     technologies: ["React", "Tailwind"],
-  },  
+  },
 ];
 
 const Project = () => {
   return (
     <div className="mt-10 ">
-      <h1 className="uppercase font-bold mb-5 text-center text-3xl text-white hover:text-[#efff16]" id="project">
+      <h1
+        className="uppercase font-bold mb-5 text-center text-3xl text-white hover:text-[#efff16]"
+        id="project"
+      >
         Mes Projets
       </h1>
       <div className="grid md:grid-cols-4 gap-4">
@@ -73,34 +72,42 @@ const Project = () => {
             key={projects.id}
             className="bg-base-300 p-5 h-fit rounded-xl shadow-lg "
           >
-            <img src={projects.image} alt={projects.title}
-                className=" w-full rounded-xl h-full object-cover hover:scale-105 transition-all duration-300"
+            <img
+              src={projects.image}
+              alt={projects.title}
+              className=" w-full rounded-xl h-full object-cover hover:scale-105 transition-all duration-300"
             />
             <div>
-                <h1 className="my-2 font-bold text-[#efff16] text-2xl te">
-                    {projects.title}
-                </h1>
-                <p className="text-base  text-white">
-                    {projects.description}
-                </p>
+              <h1 className="my-2 font-bold text-[#efff16] text-2xl te">
+                {projects.title}
+              </h1>
+              <p className="text-base  text-white">{projects.description}</p>
             </div>
             <div className=" flex flex-wrap gap-2 my-3">
-                {projects.technologies.map((tech) => (
-                    <span key={tech} className="badge bg-[#efff16] badge-sm text-black">
-                        {tech}
-                    </span>
-                ))}
+              {projects.technologies.map((tech) => (
+                <span
+                  key={tech}
+                  className="badge bg-[#efff16] badge-sm text-black"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
             <div className=" flex gap-x-3 items-center">
-                <a href={projects.demo} className="btn bg-[#efff16] text-black hover:scale-105 transition-all hover:bg-[#efff16] duration-300 w-2/3">
-                    <FaPlay className="w-4 h-4"/>
-                    Test
-                </a>
-                <a href={projects.gitHub} className="btn btn-neutral text-[#fff] hover:scale-105 transition-all duration-300 w-1/3">
-                    <AiFillGithub className="w-4 h-4"/>
-                    GitHub
-
-                </a>
+              <a
+                href={projects.demo}
+                className="btn bg-[#efff16] text-black hover:scale-105 transition-all hover:bg-[#efff16] duration-300 w-2/3"
+              >
+                <FaPlay className="w-4 h-4" />
+                Test
+              </a>
+              <a
+                href={projects.gitHub}
+                className="btn btn-neutral text-[#fff] hover:scale-105 transition-all duration-300 w-1/3"
+              >
+                <AiFillGithub className="w-4 h-4" />
+                GitHub
+              </a>
             </div>
           </div>
         ))}
